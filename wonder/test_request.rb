90c0d9f0f91d84c15c58d4ed2216c469d1346cef
Boris <gborich17@gmail.com>
2018-12-02 02:59:20 +0300

    почти полностью доделал поле чудес, кроме пары функций

--- wonder/test_request.rb
+++ wonder/test_request.rb
@@ -2,13 +2,17 @@
 # you need to install gem 'httparty' before. Enter 'gem install httparty' in Linux console for installation
 require 'httparty' #library which makes creating request easier
 site = 'http://rubyboosters.herokuapp.com/halloffame'
+
+name = 'Mr Putin'
+score = 55
+
 #hash-parameters for post request
 post_options = {
     body:
         {
-            password: 'your_original_cloud9_password',
-            user: 'Vasiliy Sikorskiy',
-            score: 25
+            password: '7wxrY3QcWJ+7',
+            user: name,
+            score: score
         }
 }
 
@@ -17,20 +21,27 @@ get_options = {
     body:
         {
             password: '7wxrY3QcWJ+7',
-            user: 'Vasiliy Sikorskiy'
+            user: name
         }
 }
+
 #this string sends user name and score to database and stores response in variable
-#response = HTTParty.post(site, post_options)
-#puts JSON.parse(response.body)
-#you can change some values:
-post_options[:body][:score] = 2
-#one more data sending
-#response = HTTParty.post(site, post_options)
-#puts JSON.parse(response.body)
+response = HTTParty.post(site, post_options)
+puts JSON.parse(response.body)
+# #you can change some values:
+# post_options[:body][:score] = 2
+# #one more data sending
+# response = HTTParty.post(site, post_options)
+# puts JSON.parse(response.body)
 #here we get current Hall of fame for some user
 response = HTTParty.get(site, get_options)
-p response.headers
+#p response.headers
 arr = response.to_a
-puts arr[0]
+puts arr.size
+a = 0
+while a < arr.size
+  puts "User: #{arr[a]['user']} | Score: #{arr[a]['score']}"
+  a += 1
+end
+#arr.each { |a| puts arr[a]}
 puts "User: #{arr[0]['user']}\nScore: #{arr[0]['score']}"
--- wonder/wonder.rb
+++ wonder/wonder.rb
@@ -1,6 +1,31 @@
 words = File.readlines('noun_dictionary.txt').each {|l| l.chomp!}
-
+require 'httparty'
 require "google/cloud/translate"
+
+site = 'http://rubyboosters.herokuapp.com/halloffame'
+
+name = 'Mr. Putin'
+score = 0
+#hash-parameters for post request
+post_options = {
+    body:
+        {
+            password: '7wxrY3QcWJ+7',
+            user: name,
+            score: score
+        }
+}
+
+#hash-parameters for get request
+get_options = {
+    body:
+        {
+            password: '7wxrY3QcWJ+7',
+            user: name
+        }
+}
+
+
 
 # Your Google Cloud Platform project ID
 project_id = "translation-01-220716"
@@ -17,19 +42,50 @@ target = "ru"
 # Translates some text into Russian
 translation = translate.translate text, to: target
 
-puts "Text: #{text}"
-puts "Translation: #{translation}"
 
 life = 3
 stars = ('*' * text.size).split('')
+
+print 'Введите ваше имя: '
+name = gets.chomp
+
+puts "Правила игры\nЗа угаданное слово сразу вам дается 100 очков, но если вы ошибетесь, то вы проиграли.
+Если вы угадываете по буквам, то за каждую верно угаданную букву вы получаете по 5 очков, и у вас есть право на 3 ошибки."
+
+print "Посмотреть таблицу лидеров? '1' - да, '0' - нет."
+ans = gets.to_i
+a = 0
+response = HTTParty.get(site, get_options)
+arr = response.to_a
+if ans == 1
+  while a < arr.size
+    puts "User: #{arr[a]['user']} | Score: #{arr[a]['score']}"
+    a += 1
+  end
+end
+puts "Text: #{text}"
+puts "Загаданное слово: #{translation}"
+print "#{name}, Вы можете попробовать угадать слово сразу(для этого введите '1'), либо можете угадывать по буквам(для этого введите '2') "
+a = gets.to_i
+
+#puts "Text: #{text}"
+#puts "Translation: #{translation}"
 puts stars.join
 
-print "Вы можете попробовать угадать слово сразу(для этого введите '1'), либо можете угадывать по буквам(для этого введите '2') "
-a = gets.to_i
 if a == 1
   print 'Введите слово: '
   word = gets.chomp
   if word == text
+    score += 100
+    response = HTTParty.post(site, post_options = {
+        body:
+            {
+                password: '7wxrY3QcWJ+7',
+                user: name,
+                score: score
+            }
+    })
+    puts "У вас #{score} очков"
     abort 'Вы выиграли!'
   else
     abort 'Вы проиграли!'
@@ -39,6 +95,8 @@ else
   print'Введите букву: '
   letter = gets.chomp
     if text.include?(letter)
+      score += 5
+      puts "У вас #{score} очков"
       puts 'Есть такая буква'
       for n in (0..text.size) do
         if text[n] == letter
@@ -56,6 +114,15 @@ else
       end
     end
     if !stars.include?('*')
+      response = HTTParty.post(site, post_options = {
+          body:
+              {
+                  password: '7wxrY3QcWJ+7',
+                  user: name,
+                  score: score
+              }
+      })
+      puts "У вас #{score} очков"
       abort 'Вы выиграли!'
     end
   end
